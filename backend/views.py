776cc30f2c448771c858e72ac1d4325440139d93
mohaalshawi <moha.alshawi@hotmail.com>
2025-06-30 15:21:15 +0300

    Add views.py from backend

---
+++ backend/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
